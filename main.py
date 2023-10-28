d8a9eb5613c3ac53bcbd5514fb7505546e11ca32
Khetog <k.dzebisov@mail.ru>
2023-10-28 05:24:16 +0300

    Repositoriy dlya Bota

---
+++ main.py
@@ -0,0 +1,46 @@
+import telebot
+from telebot import types
+
+# Замените '%ваш токен%' на ваш токен, который вы получили от BotFather
+bot = telebot.TeleBot('6812184370:AAGObPZ6foYyZHroBHSCo1VT-kBPJKXQVBE')
+
+name = ''
+surname = ''
+age = 0
+@bot.message_handler(content_types=['text', 'reg'])
+def start(message):
+    if message.text == '/reg':
+        bot.send_message(message.chat.id, "Как тебя зовут?")
+        bot.register_next_step_handler(message, get_name) #следующий шаг – функция get_name
+    else:
+        bot.send_message(message.chat.id, 'Напиши /reg')
+
+def get_name(message): #получаем фамилию
+    global name
+    name = message.text
+    bot.send_message(message.chat.id, 'Какая у тебя фамилия?')
+    bot.register_next_step_handler(message, get_surname)
+
+def get_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.chat.id,'Сколько тебе лет?')
+    bot.register_next_step_handler(message, get_age)
+
+def get_age(message):
+    global age
+    while age == 0: #проверяем что возраст изменился
+        try:
+            age = int(message.text)  # проверяем, что возраст введен корректно
+        except Exception:
+            bot.send_message(message.from_user.id, 'Цифрами, пожалуйста')
+    keyboard = types.InlineKeyboardMarkup()  # наша клавиатура
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')  # кнопка «Да»
+    keyboard.add(key_yes)  # добавляем кнопку в клавиатуру
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = 'Тебе ' + str(age) + ' лет, тебя зовут ' + name + ' ' + surname + '?'
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+
+bot.polling(none_stop=True)
